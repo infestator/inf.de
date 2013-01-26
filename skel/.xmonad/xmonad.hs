f3471f36a65ba27ec03bc084562ad31b177b6b24
Alexander Betaev <abetaev@griddynamics.com>
2013-01-26 12:27:02 +0400

    Update xmonad configuration

--- skel/.xmonad/xmonad.hs
+++ skel/.xmonad/xmonad.hs
@@ -1,26 +1,38 @@
-import XMonad
 import Control.Monad (liftM2)
+import Data.Ratio ((%))
+import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
-import XMonad.Layout.Tabbed
 import XMonad.Layout.Accordion
-import XMonad.Layout.TwoPane
 import XMonad.Layout.Combo
+import XMonad.Layout.Grid
+import XMonad.Layout.IM
+import XMonad.Layout.PerWorkspace
+import XMonad.Layout.Tabbed
+import XMonad.Layout.TwoPane
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
+import System.Cmd (system)
+import System.Environment (getEnv)
+import System.Exit (exitWith, ExitCode(..) )
 
 import XMonad.Util.Dmenu
 
 myManageHook = composeAll
     [ className =? "Idea" --> doShift "3:dev"
     , className =? "Iceweasel" --> doShift "2:web"
+    , className =? "surf" --> doShift "2:web"
     , className =? "Skype" --> doShift "4:comm"
     ]
 
 tabbedLayout = tabbed shrinkText defaultTheme
 
-myLayoutHook = avoidStruts $ tabbedLayout ||| combineTwo (TwoPane 0.03 0.5) tabbedLayout tabbedLayout
+myLayoutHook = avoidStruts $
+    onWorkspace "4:comm" (withIM (1%1) (ClassName "Skype") Grid) $
+    tabbedLayout ||| combineTwo (TwoPane 0.03 0.5) tabbedLayout tabbedLayout
+
+-- Multimedia keys definitions
 
 xK_MasterLower=(0,0x1008ff11)
 xK_MasterRaise=(0,0x1008ff13)
@@ -30,6 +42,13 @@ xK_CaptureLower=(mod4Mask,0x1008ff11)
 xK_CaptureRaise=(mod4Mask,0x1008ff13)
 xK_CaptureMute=(mod4Mask,0x1008ff12)
 
+exitHook :: IO ()
+exitHook = do
+    system "killall trayer"
+    return ()
+
+-- Main configuration
+
 main = do
     spawn "/usr/bin/xscreensaver -no-splash"
     spawn "/usr/bin/trayer --edge top --align right --widthtype percent --width 10 --height 20 --transparent true --alpha 0 --tint 0x000000 --SetDockType true --SetPartialStrut true"
@@ -37,8 +56,6 @@ main = do
     xmonad $ defaultConfig
         { workspaces = ["1:home", "2:web", "3:dev", "4:comm", "5:media", "6:tmp", "7", "8", "9", "0", "-", "="]
         , manageHook = myManageHook <+> manageDocks <+> manageHook defaultConfig
-        --, layoutHook = avoidStruts $ (Full ||| Accordion ||| tabbed shrinkText defaultTheme ||| combineTwo (TwoPane 0.03 0.5) (tabbed shrinkText defaultTheme) (tabbed shrinkText defaultTheme))
-        --, layoutHook = avoidStruts $ layoutHook defaultConfig
         , layoutHook = myLayoutHook
         , logHook = dynamicLogWithPP xmobarPP
                        { ppOutput = hPutStrLn xmproc
@@ -47,12 +64,19 @@ main = do
         , modMask = mod4Mask
         } `additionalKeys`
         [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
+        -- Screenshot keys
         , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
         , ((0, xK_Print), spawn "scrot")
-        , (xK_MasterLower, spawn "amixer -c 0 set Master 1- unmute")
-        , (xK_MasterRaise, spawn "amixer -c 0 set Master 1+ unmute")
-        , (xK_MasterMute, spawn "amixer -c 0 set Master toggle")
-        , (xK_CaptureLower, spawn "amixer -c 0 set Capture 1- unmute")
-        , (xK_CaptureRaise, spawn "amixer -c 0 set Capture 1+ unmute")
-        , (xK_CaptureMute, spawn "amixer -c 0 set Capture toggle")
+        -- Special clipboard keys
+        , ((mod4Mask, xK_c), spawn "xsel | xclip -i -selection clipboard")
+        , ((mod4Mask, xK_v), spawn "xclip -o -selection clipboard")
+        -- Multimedia keys
+        , (xK_MasterLower, spawn "amixer set Master 1- unmute")
+        , (xK_MasterRaise, spawn "amixer set Master 1+ unmute")
+        , (xK_MasterMute, spawn "amixer set Master toggle")
+        , (xK_CaptureLower, spawn "amixer set Capture 1- unmute")
+        , (xK_CaptureRaise, spawn "amixer set Capture 1+ unmute")
+        -- SESSION
+--        , ((mod4Mask, xK_q), io (system "touch ~/.exit_flag" >> exitHook >> exitWith ExitSuccess))
+--        , ((mod4Mask, xK_q), io exitHook >> restart "xmonad" True)
         ]
