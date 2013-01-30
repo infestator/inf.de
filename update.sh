1ac1deb1390a0630225254e94821f48de58c218a
Alexander Betaev <abetaev@griddynamics.com>
2013-01-30 11:08:21 +0400

    Update .xmobarrc to color battery charge; change update.sh script to work the way install.sh does

--- update.sh
+++ update.sh
@@ -3,21 +3,19 @@
 SKEL=${1}
 
 if [ -n "${SKEL}" ] | [ ! -d "${SKEL}" ]; then
-	echo "Syntax: $(basename "${SKEL}") <skel_dir>"
+	echo "Syntax: $(basename "${0}") <skel_dir>"
 	exit
 fi
 
-OLD_PWD=${PWD}
-cd ${SKEL}
-for FILE in $(find -type f); do
-    FILE="$(expr substr "${FILE}" 3 $(($(expr length "${FILE}")-2)))"
-    mkdir -p "$(dirname "${FILE}")"
-    echo ${FILE}
-    if [ -f ${HOME}/${FILE} ]; then
-        diff "${HOME}/${FILE}" "${FILE}"
-    	cp -iv "${HOME}/${FILE}" "${FILE}"
-    else
-        rm -iv "${FILE}"
+for FILE in $(find ${SKEL} -type f | sed -e "s#${SKEL}/##"); do
+    DST="${SKEL}/${FILE}"
+    SRC="${HOME}/${FILE}"
+    if [ ! -f "${SRC}" ]; then
+        rm -v ${DST}
+    elif [ "$(diff ${SRC} ${DST} | wc -l)" != "0" ]; then
+        if [ ! -d ${DST_DIR} ]; then
+            mkdir -p ${DST_DIR}
+        fi
+    	cp -v ${SRC} ${DST}
     fi
 done
-cd ${OLD_PWD}
