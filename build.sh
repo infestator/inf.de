ccfeeee616d68c81e9ff1abc969e411f44b395a6
Alexander Betaev <abetaev@griddynamics.com>
2013-03-19 01:08:56 +0400

    Build script update; py-launcher script update

--- build.sh
+++ build.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
+echo "Clean"
+rm -rf build
 
+echo "Create directories"
+mkdir -p build/bin
+mkdir -p build/share/debb/python
+
+echo "Copy files"
+cp -r debb.py/src/* build/share/debb/python
+cp -d debb.sh/* build/bin
+cp -r debb.meta/* build/share
+
+echo "Done"
--- debb.sh/debb-py-launcher.sh
+++ debb.sh/debb-py-launcher.sh
@@ -1,12 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ -z "${DEBB_HOME}" ]; then
 	DEBB_HOME=$(dirname $(dirname $0))
+	if [ "${DEBB_HOME}" == "." ]; then
+		DEBB_HOME=$(dirname $(pwd))
+	fi
 fi
 
 PYTHON_VERSION=$(python --version 2>&1 | sed -re 's/^Python ([0-9]+\.[0-9]+)\.[0-9]+$/\1/')
 
-export PYTHON_DEBB_OATH=${DEBB_HOME}/share/debb/python
-export PYTHON_PATH=${PYTHON_DEBB_PATH}:${PYTHON_PATH}
+export DEBB_PYTHONPATH=${DEBB_HOME}/share/debb/python
+export PYTHONPATH=${DEBB_PYTHONPATH}:${PYTHON_PATH}
 
-python ${PYTHON_DEBB_PATH}/${SCRIPT}
+IFS='-' read -a BASENAME <<< "$(basename "$0")"
+
+NAME="${BASENAME[1]}"
+TYPE="${BASENAME[2]}"
+
+if [ -z "${TYPE}" ]; then
+	SCRIPT="debb/${NAME}.py"
+else
+	SCRIPT="debb/${TYPE}/${NAME}.py"
+fi
+
+#echo $PYTHON_PATH
+
+python "${DEBB_PYTHONPATH}/${SCRIPT}"
